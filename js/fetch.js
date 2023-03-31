9bda00546d4b7c190695e4e7b4bbdf49933af1ac
aulianafahrian <aulianafahrian@gmail.com>
2023-03-31 16:18:18 +0700

    komiu

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-let urlAPI = "https://aufa-ulbi.herokuapp.com";
+let urlAPI = "https://aufa-ulbi.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
