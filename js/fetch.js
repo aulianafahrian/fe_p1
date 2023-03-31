a0935aa9389d8d432bfe5a67a7563632822c449c
aulianafahrian <aulianafahrian@gmail.com>
2023-03-31 17:08:58 +0700

    k

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,10 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://aufa-ulbi.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
+
 function isiTablePresensi(results){
     console.log(results);
     results.forEach(isiRow);
