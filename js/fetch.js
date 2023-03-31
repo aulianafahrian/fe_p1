629e60651bd41d6dae37c80936a63ca7104209e4
aulianafahrian <aulianafahrian@gmail.com>
2023-03-31 17:03:16 +0700

    komit

--- js/fetch.js
+++ js/fetch.js
@@ -6,5 +6,14 @@ function isiTablePresensi(results){
     results.forEach(isiRow);
 }
 function isiRow(value){
-    console.log(value)
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
