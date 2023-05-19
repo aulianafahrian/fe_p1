82e18c491092647928fc6decac09b28b5303bcef
aulianafahrian <aulianafahrian@gmail.com>
2023-05-19 15:29:44 +0700

    sa

--- js/fetch_edit.js
+++ js/fetch_edit.js
@@ -1,5 +1,5 @@
 import{get} from "https://bukulapak.github.io/apo/process.js";
-import { isiData } from "./controller/edit";
+import { isiData } from "./controller/edit.js";
 import { urlFetch } from "./config/url_get_detil";
 
 get(urlFetch, isiData)
