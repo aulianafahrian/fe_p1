8097dc3e810423b701c7cefbf57fa5d1564cca28
aulianafahrian <aulianafahrian@gmail.com>
2023-05-19 15:39:09 +0700

    asda

--- js/fetch_edit.js
+++ js/fetch_edit.js
@@ -1,5 +1,5 @@
 import{get} from "https://bukulapak.github.io/api/process.js";
 import { isiData } from "./controller/edit.js";
-import { urlFetch } from "./config/url_get_detil";
+import { urlFetch } from "./config/url_get_detil.js";
 
 get(urlFetch, isiData)
